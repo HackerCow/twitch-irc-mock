46f5ab0254959c9dbbf8452627f9021be1292f68
Christoph Böhmwalder <christoph@boehmwalder.at>
2016-08-15 15:15:03 +0200

    fix compilation error (oops)

--- twitch-irc-mock/Handlers/Join.cs
+++ twitch-irc-mock/Handlers/Join.cs
@@ -13,20 +13,21 @@ namespace twitch_irc_mock
 					return new IrcResponse[] {};
 				}
 
-			if (!IrcChannel.Channels.ContainsKey(args[0]))
-			{
-				IrcChannel.Channels.Add(args[0], new IrcChannel(args[0]));
-			}
+				if (!IrcChannel.Channels.ContainsKey(args[0]))
+				{
+					IrcChannel.Channels.Add(args[0], new IrcChannel(args[0]));
+				}
 
-			IrcChannel.Channels[args[0]].Users.Add(session);
-			session.CurrentChannel = args[0];
-			
-			return new IrcResponse[]
-			{
-				new EchoResponse(session, "JOIN " + args[0]),
-				new CustomResponse(session, $":{session.Nick}.{Config.Hostname} 353 {session.Nick} = {args[0]} :{session.Nick}"),
- 				new CustomResponse(session, $":{session.Nick}.{Config.Hostname} 366 {session.Nick} {args[0]} :End of /NAMES list"),
-			};
+				IrcChannel.Channels[args[0]].Users.Add(session);
+				session.CurrentChannel = args[0];
+
+				return new IrcResponse[]
+				{
+					new EchoResponse(session, "JOIN " + args[0]),
+					new CustomResponse(session, $":{session.Nick}.{Config.Hostname} 353 {session.Nick} = {args[0]} :{session.Nick}"),
+					new CustomResponse(session, $":{session.Nick}.{Config.Hostname} 366 {session.Nick} {args[0]} :End of /NAMES list"),
+				};
+			}
 		}
 	}
 }
